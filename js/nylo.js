8696bcc1701360eac3dfab2ea949fdd68886a99c
veggero <niccolo.venerandi@gmail.com>
2017-12-11 21:45:32 +0100

    yeppp

--- js/nylo.js
+++ js/nylo.js
@@ -1,5 +1,5 @@
 var last = -1;
-var code = "@// github.com/veggero/nylo@@fib:@    int n |@    =0 or =1@        1@    else@        fib(n-1)+fib(n-2)@";
+var code = "@// github.com/veggero/nylo@@fib:@    int n |@    =0 or =1@        1@    else@        fib(n-1)+fib(n-2)";
 var nl = 0;
 var topy = '50%';
 
@@ -7,6 +7,7 @@ var write = function(x) {
 	last+=1;
 	last_char = code[last]
 	if (last_char == ' ') {last_char = '&nbsp;'}
+	if (last_char == undefined) {return}
 	if (last_char=="@") {nl+=1; x.innerHTML+="<br>"; x.style.left="50%"; x.style.top = "calc( "+topy+" - 200px - "+(nl*39)+"px)"}
 	else {x.innerHTML+=last_char};
 	x.style.left = (x.getBoundingClientRect().left-15)+"px";
